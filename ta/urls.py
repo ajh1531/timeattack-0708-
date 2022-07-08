f26d08b6a52fb8fd715dd8ed07101ba90a271171
ajh1531 <85057618+ajh1531@users.noreply.github.com>
2022-07-08 16:34:43 +0900

    time_attack 추가

---
+++ ta/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('user/', include('user.urls')),
+
+    path('post/', include('post.urls')),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+
+]
